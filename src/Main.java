cc9734f2e2a894a87b5a80b62897e0d822c454b1
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-08 23:12:00 +0800

    feat: add main menu

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-import com.elements.mainMenu.*;
 
 
 public class Main {
     public static void main(String[] args){
-
-        mainMenuModel menuModel = new mainMenuModel();
-        mainMenuView menuView = new mainMenuView();
-        mainMenuController menuController = new mainMenuController(menuModel, menuView);
-
-
-
-        int choice = menuController.start();
-
+        System.out.println("-------------------------------");
+        System.out.println("Trading Card Inventory System:");
+        System.out.println("-------------------------------");
+        System.out.println("[1] Add Card");
+        System.out.println("[2] Manage Card Count");
+        System.out.println("[3] Create New Binder");
+        System.out.println("[4] Manage Binder");
+        System.out.println("[5] Create New Deck");
+        System.out.println("[6] Manage Deck");
     }
 }
