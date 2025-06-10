b73397dc0f99411084c8286f5bbffa8764acfaec
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-11 01:36:03 +0800

    feat: added collectoin

--- src/com/tradingcards/elements/collection/CollectionModel.java
+++ src/com/tradingcards/elements/collection/CollectionModel.java
@@ -1,4 +1,17 @@
 package com.tradingcards.elements.collection;
 
+import com.tradingcards.elements.card.CardModel;
+import java.util.HashMap;
+
 public class CollectionModel {
+
+    private static HashMap<String, CardModel> collection = new HashMap<>();
+
+    public static void setCollection(CardModel card, String name) {
+        collection.put(name, card);
+    }
+
+    public static HashMap<String, CardModel> getCollection() {
+        return collection;
+    }
 }
