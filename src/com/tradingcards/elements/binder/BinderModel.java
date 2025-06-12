79a7f8b9d6efc6610bfd8bcf173716d72fd9f55e
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-06-12 18:20:58 +0800

    feat: setup binder model and collection model

--- src/com/tradingcards/elements/binder/BinderModel.java
+++ src/com/tradingcards/elements/binder/BinderModel.java
@@ -6,8 +6,8 @@ import java.util.HashMap;
 public class BinderModel {
 
     private String binderName;
-    private int cardsInBinder = 0; //have a get set for this variable
-    private CardModel[] cards = new CardModel[20]; //have a get set for this variable, also try to make this as array list if you can but this is ok naman, or... maybe hashmap padin since i think we would be gettinig the card using name ulet...
+    private int cardsInBinderQuantity = 0; //have a get set for this variable
+    private HashMap<String, CardModel> cardsInBinder = new HashMap<>();
 
     public void setName(String binderName) {
         this.binderName = binderName;
@@ -18,34 +18,26 @@ public class BinderModel {
     }
 
     //first of all these should be in the controller
-    public void deleteBinder(HashMap<String, CardModel> collection) {
-        for (int i = 0; i < cardsInBinder; i++) {
-            collection.put(cards[i].getName(), cards[i]);
-            cards[i] = null;
-        }
-    }
-
-    public void addCard(CardModel card) {
-        //Not sure if addCard should handle removal from collection as well
-        if (cardsInBinder != 20) {
-            cards[cardsInBinder] = card;
-            this.cardsInBinder += 1;
-        }
-
-    }
-
-    public void removeCard(CardModel card) {
-
-        if (cardsInBinder != 0) {
-            //Incomplete, does not remove anything from cards array yet
-            //Should this insert the card to the collection through parameter?
-            this.cardsInBinder -= 1;
-        }
-
-    }
-
-    public void tradeCard(CardModel cardInDeck, CardModel cardOutsideDeck) {
-
-    }
-
+    // public void deleteBinder(HashMap<String, CardModel> collection) {
+    //     for (int i = 0; i < cardsInBinder; i++) {
+    //         collection.put(cards[i].getName(), cards[i]);
+    //         cards[i] = null;
+    //     }
+    // }
+    // public void addCard(CardModel card) {
+    //     //Not sure if addCard should handle removal from collection as well
+    //     if (cardsInBinder != 20) {
+    //         cards[cardsInBinder] = card;
+    //         this.cardsInBinder += 1;
+    //     }
+    // }
+    // public void removeCard(CardModel card) {
+    //     if (cardsInBinder != 0) {
+    //         //Incomplete, does not remove anything from cards array yet
+    //         //Should this insert the card to the collection through parameter?
+    //         this.cardsInBinder -= 1;
+    //     }
+    // }
+    // public void tradeCard(CardModel cardInDeck, CardModel cardOutsideDeck) {
+    // }
 }
--- src/com/tradingcards/elements/collection/CollectionModel.java
+++ src/com/tradingcards/elements/collection/CollectionModel.java
@@ -1,11 +1,14 @@
 package com.tradingcards.elements.collection;
 
+import com.tradingcards.elements.binder.BinderModel;
 import com.tradingcards.elements.card.CardModel;
 import java.util.HashMap;
 
 public class CollectionModel {
 
     private HashMap<String, CardModel> cardCollection = new HashMap<>();
+
+    private HashMap<String, BinderModel> binderCollection = new HashMap<>();
 
     public void setCardCollection(CardModel card, String name) {
         cardCollection.put(name, card);
@@ -14,4 +17,13 @@ public class CollectionModel {
     public HashMap<String, CardModel> getCardCollection() {
         return cardCollection;
     }
+
+    public void setBinderCollection(CardModel card, String name) {
+        cardCollection.put(name, card);
+    }
+
+    public HashMap<String, BinderModel> getBinderCollection() {
+        return binderCollection;
+    }
+
 }
