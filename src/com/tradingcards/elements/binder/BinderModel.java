b5fe47ffab0e2a6309c119275d908dcb5f4ed76d
nathan <nathangabrieldolot@gmail.com>
2025-06-23 14:23:12 +0800

    removed unused comments

--- src/com/tradingcards/elements/binder/BinderModel.java
+++ src/com/tradingcards/elements/binder/BinderModel.java
@@ -45,31 +45,4 @@ public class BinderModel {
     public void insertInBinder(CardModel card, String name){
         cardsInBinder.put(name, card);
     }
-
-
-
-
-    // first of all these should be in the controller
-    // public void deleteBinder(HashMap<String, CardModel> collection) {
-    //     for (int i = 0; i < cardsInBinder; i++) {
-    //         collection.put(cards[i].getName(), cards[i]);
-    //         cards[i] = null;
-    //     }
-    // }
-    // public void addCard(CardModel card) {
-    //     // Not sure if addCard should handle removal from collection as well
-    //     if (cardsInBinder != 20) {
-    //         cards[cardsInBinder] = card;
-    //         this.cardsInBinder += 1;
-    //     }
-    // }
-    // public void removeCard(CardModel card) {
-    //     if (cardsInBinder != 0) {
-    //         // Incomplete, does not remove anything from cards array yet
-    //         // Should this insert the card to the collection through parameter?
-    //         this.cardsInBinder -= 1;
-    //     }
-    // }
-    // public void tradeCard(CardModel cardInDeck, CardModel cardOutsideDeck) {
-    // }
 }
