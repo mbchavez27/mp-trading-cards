a2eb85f81184c012e311f9dd4851c62b4e49ea8f
nathan <nathangabrieldolot@gmail.com>
2025-07-16 15:06:33 +0800

    Created basic design for menu

--- app/src/main/java/com/tradingcards/Main.java
+++ app/src/main/java/com/tradingcards/Main.java
@@ -12,41 +12,44 @@
 
 package com.tradingcards;
 
-import com.tradingcards.elements.menus.MainMenuView;
-
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import src.main.java.com.tradingcards.elements.menus.SceneManager;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
 
-        MainMenuView menuView = new MainMenuView(stage);
-
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.CENTER);
         vbox.setSpacing(20);
+
+        Scene homeScene = new Scene(vbox, 400, 300);
+        stage.setScene(homeScene);
+        stage.setTitle("Trading Cards App");
+
+        com.tradingcards.elements.menus.MainMenu menuView = new com.tradingcards.elements.menus.MainMenu(stage, homeScene);
+
+        //Functionality for button 1
         Button button1 = new Button("Start");
         button1.setOnAction(e -> {
             stage.setScene(menuView.getScene());
         });
 
-        Button button2 = new Button("Exit");
+        //Functionality for button 2
+        Button button2 = new Button("Close Program");
+        button2.setOnAction(e -> stage.close());
 
-
-        Label title = new Label("Trading Card Inventory System");
+        Label title = new Label("    Trading Card\n Inventory System");
         vbox.getChildren().addAll(title, button1, button2);
 
-        Scene scene = new Scene(vbox, 400, 300);
-        stage.setScene(scene);
-        stage.setTitle("Trading Cards App");
+        homeScene.getStylesheets().add(getClass().getResource("/styles/HomeMenu.css").toExternalForm());
+
+        //Displays stage
         stage.show();
     }
 
--- app/src/main/java/com/tradingcards/elements/menus/MainMenu.java
+++ app/src/main/java/com/tradingcards/elements/menus/MainMenu.java
@@ -12,14 +12,14 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * Controller class for handling the main menu of the Trading Card Inventory
  * System. Initializes and coordinates all component controllers and handles
  * user input to navigate the system.
  */
-public class MainMenuView {
+public class MainMenu {
 
     /** Shared collection that holds cards, binders, and decks. */
     private CollectionModel sharedCollection = new CollectionModel();
@@ -32,7 +32,10 @@ public class MainMenuView {
 
     private Scene scene;
 
-    public MainMenuView(Stage stage){
+    public MainMenu(Stage stage, Scene home){
+
+        ArrayList<Button> buttonCollection = new ArrayList<>();
+
         CardView cardView = new CardView();
         CardController cardController = new CardController(sharedCollection, cardView);
 
@@ -42,20 +45,47 @@ public class MainMenuView {
         DeckView deckView = new DeckView();
         DeckController deckController = new DeckController(sharedCollection, deckView);
 
+
         Button addCardBtn = new Button("Add Card");
         addCardBtn.setOnAction(e -> System.out.println("ADD CARD"));
+        buttonCollection.add(addCardBtn);
+
 
         Button addBinderBtn = new Button("Add Binder");
         addBinderBtn.setOnAction(e -> System.out.println("ADD BINDER"));
+        buttonCollection.add(addBinderBtn);
 
         Button addDeckBtn = new Button("Add Deck");
         addDeckBtn.setOnAction(e -> System.out.println("ADD Deck"));
+        buttonCollection.add(addDeckBtn);
 
-        Button exitBtn = new Button("Exit Menu");
+        Button exitBtn = new Button("Return to home Menu");
+        exitBtn.setOnAction(e -> {
+            stage.setScene(home);
+        });
+        buttonCollection.add(exitBtn);
+
+        if (!sharedCollection.getCardCollection().isEmpty()) {
+            Button manageCardsBtn = new Button("Manage Cards Menu");
+            manageCardsBtn.setOnAction(e -> System.out.println("Manage Cards Menu"));
+            buttonCollection.add(manageCardsBtn);
+        }
+        if (!sharedCollection.getBinderCollection().isEmpty()) {
+            Button manageBindersBtn = new Button("Manage Binders Menu");
+            manageBindersBtn.setOnAction(e -> System.out.println("Manage Binders Menu"));
+            buttonCollection.add(manageBindersBtn);
+        }
+        if (!sharedCollection.getDeckCollection().isEmpty()) {
+            Button manageDecksBtn = new Button("Manage Decks Menu");
+            manageDecksBtn.setOnAction(e -> System.out.println("Manage Decks Menu"));
+            buttonCollection.add(manageDecksBtn);
+        }
 
         HBox hbox = new HBox();
-        hbox.getChildren().addAll(addCardBtn, addBinderBtn, addDeckBtn);
+        //adds all elements to hbox
+        hbox.getChildren().addAll(buttonCollection);
         scene = new Scene(hbox, 500,500);
+        scene.getStylesheets().add(getClass().getResource("/styles/MainMenu.css").toExternalForm());
     }
 
     public Scene getScene(){
@@ -125,6 +155,9 @@ public class MainMenuView {
     /**
      * Displays the base menu options to the user.
      * The options dynamically reflect the current state of the collection.
+     */
+    /*
+
      */
     private void baseMenu() {
         System.out.println("[1] Add Card");
